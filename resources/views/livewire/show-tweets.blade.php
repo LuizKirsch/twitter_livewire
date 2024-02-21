70bf5bd1401d4855a02648a4579c47ed84699166
lluiziull <luizkirsch0@gmail.com>
2024-02-21 08:25:58 -0300

    feat: paginificação

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,16 +1,33 @@
 <div>
     tweets
 
-    <p>
-        {{$message}}
-    </p>
+    {{-- <p>
+        {{$content}}
+    </p> --}}
     
-    <label for="live-message">live-message</label>
-    <input type="text" wire:model.live="message" id="live-message"> 
+    {{-- <label for="live-content">live-content</label>
+    <input type="text" wire:model.live="content" id="live-content">  --}}
+
+    <form action="" method="post" wire:submit.prevent="create" >
+        <label for="content">tweet</label>
+        <input type="text" wire:model.live="content" id="content" name="content">
+
+        @error('content')
+            {{$message}}
+        @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
 
     <hr>
 
     @foreach ($tweets as $tweet)
         {{$tweet->user->name}} - {{$tweet->content}} <br>
     @endforeach
+
+    <hr>
+
+    <div>
+        {{$tweets->links()}}
+    </div>
+    
 </div>
